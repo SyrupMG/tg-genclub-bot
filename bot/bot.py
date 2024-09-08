9ae54a5caa4764a8d3c98d31138948ed3821fcd6
Alexey Lysenko <abesmon@gmail.com>
2024-09-08 19:49:10 +0300

    update

--- bot/bot.py
+++ bot/bot.py
@@ -1,21 +1,25 @@
 import os
-from telegram.ext import Updater, CommandHandler
 
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! I'm your Telegram bot.")
+from telegram import Update
+from telegram.ext import Application, CommandHandler
+
+async def start(update, context):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! I'm your Telegram bot.")
 
 def main():
     token = os.environ.get('TELEGRAM_BOT_TOKEN')
     if not token:
         raise ValueError("No token provided")
 
-    updater = Updater(token, use_context=True)
-    dp = updater.dispatcher
+    """Start the bot."""
+    # Create the Application and pass it your bot's token.
+    application = Application.builder().token(token).build()
 
-    dp.add_handler(CommandHandler("start", start))
+    # on different commands - answer in Telegram
+    application.add_handler(CommandHandler("start", start))
 
-    updater.start_polling()
-    updater.idle()
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 if __name__ == '__main__':
     main()
