326eb6ba33232ece14e68b24a5850294e23d9a7c
Alexey Lysenko <abesmon@gmail.com>
2024-09-09 14:11:53 +0300

    demo for pip image generation

--- bot/bot.py
+++ bot/bot.py
@@ -6,6 +6,26 @@ from telegram.ext import Application, CommandHandler
 async def start(update, context):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! I'm your Telegram bot. And this was updated from git")
 
+async def draw_circle(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    WIDTH = 512
+    HEIGHT = 512
+
+    img = Image.new('RGB', (WIDTH, HEIGHT), color='white')
+    draw = ImageDraw.Draw(img)
+    
+    center_x = random.randint(0, WIDTH)
+    center_y = random.randint(0, HEIGHT)
+    radius = random.randint(10, WIDTH / 2)
+    color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    
+    draw.ellipse([center_x-radius, center_y-radius, center_x+radius, center_y+radius], fill=color)
+    
+    img_byte_arr = io.BytesIO()
+    img.save(img_byte_arr, format='PNG')
+    img_byte_arr.seek(0)
+    
+    await update.message.reply_photo(img_byte_arr)
+
 def main():
     token = os.environ.get('TELEGRAM_BOT_TOKEN')
     if not token:
@@ -17,6 +37,7 @@ def main():
 
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("draw_circle", draw_circle))
 
     # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
