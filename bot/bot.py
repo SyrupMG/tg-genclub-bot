8558dd04d89847f73e010a915feb152b3a12fbd7
illus0r <zor667@gmail.com>
2024-09-10 22:41:30 +0300

    print a random silent user

--- bot/bot.py
+++ bot/bot.py
@@ -1,11 +1,16 @@
 import os
 import markovify
+import random
 from telegram import Update
-from telegram.ext import Application, CommandHandler
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from collections import defaultdict
 
 with open("markov.txt", "r", encoding="utf-8") as f:
     text = f.read()
 model = markovify.Text(text)
+
+# Dictionary to store users and their message count
+user_messages = defaultdict(int)
 
 def generate_markov_text(sentences=5):
     result = []
@@ -15,29 +20,52 @@ def generate_markov_text(sentences=5):
             result.append(sentence)
     return " ".join(result)
 
-async def start(update, context):
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! I'm your Telegram bot. And this was updated from git")
 
-async def markov(update: Update, context):
+async def markov(update: Update, context: ContextTypes.DEFAULT_TYPE):
     markov_text = generate_markov_text()
     await context.bot.send_message(chat_id=update.effective_chat.id, text=markov_text)
+
+async def silent(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.effective_chat.id
+    
+    # Get all chat members
+    chat_members = await context.bot.get_chat_administrators(chat_id)
+    
+    silent_users = []
+    for member in chat_members:
+        user = member.user
+        if user_messages[user.id] == 0 and not user.is_bot:
+            silent_users.append(user.full_name)
+    
+    if silent_users:
+        random_silent_user = random.choice(silent_users)
+        response = f"A random silent user: {random_silent_user}"
+    else:
+        response = "No silent users found."
+    
+    await context.bot.send_message(chat_id=chat_id, text=response)
+
+async def track_messages(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.message.from_user.id
+    user_messages[user_id] += 1
 
 def main():
     token = os.environ.get('TELEGRAM_BOT_TOKEN')
     if not token:
         raise ValueError("No token provided")
-
-    """Start the bot."""
-    # Create the Application and pass it your bot's token.
+    
     application = Application.builder().token(token).build()
-
-    # on different commands - answer in Telegram
+    
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("markov", markov))
-
-    # Run the bot until the user presses Ctrl-C
+    application.add_handler(CommandHandler("silent", silent))
+    
+    # Add a message handler to track user messages
+    application.add_handler(MessageHandler(filters.ALL, track_messages))
+    
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 if __name__ == '__main__':
     main()
-
